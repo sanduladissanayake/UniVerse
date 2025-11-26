60dc51436b4d530200de856cbc8c531a69685ed9
dumithmifernando <tresnadumithmi@gmail.com>
2025-11-26 22:51:05 +0530

    Club pages were added, improved the stylings

---
+++ Frontend/src/pages/ClubDetailPage.tsx
@@ -0,0 +1,12 @@
+export function ClubDetailPage() {
+  return (
+    <div className="min-h-screen pt-24 pb-16">
+      <div className="container mx-auto px-6">
+        <div className="text-center">
+          <h1 className="text-4xl font-bold text-white mb-4">Club Details</h1>
+          <p className="text-white/60">Detailed club information will be displayed here.</p>
+        </div>
+      </div>
+    </div>
+  );
+}
